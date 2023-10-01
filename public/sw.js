966f7c27dbeebbaabe762f64ee73cd6358407c94
13x54n <13x54r@gmail.com>
2023-10-01 12:56:05 -0400

    Offline mode is not rendering

--- public/sw.js
+++ public/sw.js
@@ -1,29 +1,33 @@
-// public/sw.js
-const CACHE_NAME = 'thriftmyout-v1';
+const CACHE_NAME = 'pwa-cache-v1';
 const OFFLINE_URL = '/offline.html';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+//   '/styles.css', // and other stylesheets
+//   '/script.js',  // and other scripts
+//   '/icon.png',   // and other images
+  OFFLINE_URL
+];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
-                OFFLINE_URL
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    if (event.request.mode === 'navigate') {
-        event.respondWith(
-            fetch(event.request)
-                .catch(() => {
-                    return caches.open(CACHE_NAME)
-                        .then((cache) => {
-                            return cache.match(OFFLINE_URL);
-                        });
-                })
-        );
-    } else {
-        event.respondWith(fetch(event.request));
-    }
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        // If the fetch fails, return the offline page.
+        if (event.request.mode === 'navigate') {
+          return caches.match(OFFLINE_URL);
+        }
+      })
+  );
 });
