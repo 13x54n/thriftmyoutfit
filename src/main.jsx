21508c1839bdcc8b41857fef9d5409884ae81001
13x54n <13x54r@gmail.com>
2023-10-01 12:45:45 -0400

    Working on pwa

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,35 @@ import "remixicon/fonts/remixicon.css";
 import "./index.css";
 import Router from "./utils/Routers.jsx";
 import ProductProvider from "./providers/ProductProvider";
+import { registerSW } from 'virtual:pwa-register';
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").then(
+      (registration) => {
+        console.log("ServiceWorker registration successful:", registration);
+      },
+      (error) => {
+        console.log("ServiceWorker registration failed:", error);
+      }
+    );
+  });
+}
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // show a prompt to user
+    console.log('New content available. Please refresh.');
+  },
+  onOfflineReady() {
+    // show a ready to work offline to user
+    console.log('Ready for offline use.');
+  },
+});
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <ProductProvider>
-      <Router />
-      </ProductProvider>
+  <ProductProvider>
+    <Router />
+  </ProductProvider>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,73 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import replace from '@rollup/plugin-replace';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        // content of your manifest file
+        name: 'ThriftMyOut',
+        short_name: 'TMO',
+        description: 'ThriftMyOutfit is a modern lifestyle.',
+        icons: [
+          {
+            src: "https://ik.imagekit.io/13x54r/ThrifyMyOutfit/tr:w-192,h-192/White%20Retro%20Smile%20Clothing%20Company%20Logo.png?updatedAt=1694628876023",
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: "https://ik.imagekit.io/13x54r/ThrifyMyOutfit/tr:w-512,h-512/White%20Retro%20Smile%20Clothing%20Company%20Logo.png?updatedAt=1694628876023",
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: "https://ik.imagekit.io/13x54r/ThrifyMyOutfit/tr:w-192,h-192/White%20Retro%20Smile%20Clothing%20Company%20Logo.png?updatedAt=1694628876023",
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: "maskable"
+          },
+        ],
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+      },
+      workbox: {
+        // workbox options for generating service worker
+        globPatterns: ['**/*'],
+        skipWaiting: true,
+        clientsClaim: true,
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'google-fonts-stylesheets',
+            },
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-webfonts',
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+                maxEntries: 30,
+              },
+            },
+          },
+        ],
+      },
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+    }),
+  ],
 })
